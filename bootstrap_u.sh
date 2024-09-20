372b4284aaf3e475d786759df3394974b2339fff
LGXerxes <ditisluca@yahoo.it>
2024-09-21 00:57:08 +0200

    stuf

---
+++ bootstrap_u.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update package list and install Git
+sudo pacmacn -Syu
+sudo pacman -S git
+
+# Clone the repository
+git clone https://github.com/structwafel/stuff.git
+
+# Change to the repository directory
+cd stuff
+
+# Run the install script
+./just_user_things.sh
