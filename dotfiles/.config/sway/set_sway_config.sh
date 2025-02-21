78496796a6e063d929e37642009e5098bd087ccc
LGXerxes <ditisluca@yahoo.it>
2025-02-21 01:13:56 +0100

    correct app_id for youtube music

--- dotfiles/.config/sway/set_sway_config.sh
+++ dotfiles/.config/sway/set_sway_config.sh
@@ -5,8 +5,8 @@ hostname=$(cat /etc/hostname)
 if [ "$hostname" = "xpslaptop" ]; then
     cat <<EOF > ~/.config/sway/generated_config
 exec_always "youtube-music"
-for_window [app_id="YouTube Music"] move scratchpad
-bindsym \$mod+m [app_id="YouTube Music"] scratchpad show, resize set width 1600 height 800, move position center
+for_window [app_id="com.github.th_ch.youtube_music"] move scratchpad
+bindsym \$mod+m [app_id="com.github.th_ch.youtube_music"] scratchpad show, resize set width 1600 height 800, move position center
 
 exec_always "signal-desktop"
 for_window [app_id="signal"] move scratchpad
@@ -19,8 +19,8 @@ EOF
 elif [ "$hostname" = "archswayhome" ]; then
     cat <<EOF > ~/.config/sway/generated_config
 exec_always "youtube-music"
-for_window [app_id="YouTube Music"] move scratchpad
-bindsym \$mod+m [app_id="YouTube Music"] scratchpad show, resize set width 1920 height 1080, move position center
+for_window [app_id="com.github.th_ch.youtube_music"] move scratchpad
+bindsym \$mod+m [app_id="com.github.th_ch.youtube_music"] scratchpad show, resize set width 1920 height 1080, move position center
 
 exec_always "signal-desktop"
 for_window [app_id="signal"] move scratchpad
