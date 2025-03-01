31cee8277e7ccc172577b8a2381a7b88a8f5f87d
LGXerxes <ditisluca@yahoo.it>
2025-03-01 10:33:24 +0100

    signal on desktop is overrated

--- dotfiles/.config/sway/set_sway_config.sh
+++ dotfiles/.config/sway/set_sway_config.sh
@@ -8,9 +8,9 @@ exec_always "youtube-music"
 for_window [app_id="com.github.th_ch.youtube_music"] move scratchpad
 bindsym \$mod+m [app_id="com.github.th_ch.youtube_music"] scratchpad show, resize set width 1600 height 800, move position center
 
-exec_always "signal-desktop"
-for_window [app_id="signal"] move scratchpad
-bindsym \$mod+r [app_id="signal"] scratchpad show, resize set width 1600 height 800, move position center
+# exec_always "signal-desktop"
+# for_window [app_id="signal"] move scratchpad
+# bindsym \$mod+r [app_id="signal"] scratchpad show, resize set width 1600 height 800, move position center
 
 exec_always "kitty --class mykitty ~/"
 for_window [app_id="mykitty"] move scratchpad
@@ -22,9 +22,9 @@ exec_always "youtube-music"
 for_window [app_id="com.github.th_ch.youtube_music"] move scratchpad
 bindsym \$mod+m [app_id="com.github.th_ch.youtube_music"] scratchpad show, resize set width 1920 height 1080, move position center
 
-exec_always "signal-desktop"
-for_window [app_id="signal"] move scratchpad
-bindsym \$mod+r [app_id="signal"] scratchpad show, resize set width 1920 height 1080, move position center
+# exec_always "signal-desktop"
+# for_window [app_id="signal"] move scratchpad
+# bindsym \$mod+r [app_id="signal"] scratchpad show, resize set width 1920 height 1080, move position center
 
 exec_always "kitty --class mykitty ~/"
 for_window [app_id="mykitty"] move scratchpad
