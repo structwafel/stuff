39a40f5aabd78bf0ac7ec96927424fad8a203a11
LGXerxes <ditisluca@yahoo.it>
2025-03-29 11:56:11 +0100

    neo tree stuff

---
+++ dotfiles/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,10 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "right",
+        -- Optional: Set an obvious width to see if *any* options apply
+        width = 10,
+      },
+    },
+  }
