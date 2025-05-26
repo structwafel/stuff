2965197cb896ccc8011ebaa759bc74d1044ac2c3
LGXerxes <ditisluca@yahoo.it>
2025-05-26 18:47:50 +0200

    Ctrl+c and Ctrl+v normal in wezterm

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -36,4 +36,27 @@ config.window_close_confirmation = "NeverPrompt"
 --   },
 -- }
 
+config.keys = {
+  {
+    key = 'c',
+    mods = 'CTRL',
+    action = wezterm.action_callback(function(window, pane)
+      local has_selection = window:get_selection_text_for_pane(pane) ~= ''
+      if has_selection then
+        window:perform_action(act.CopyTo 'ClipboardAndPrimarySelection', pane)
+
+        window:perform_action(act.ClearSelection, pane)
+      else
+        window:perform_action(act.SendKey { key = 'c', mods = 'CTRL' }, pane)
+      end
+    end),
+  },
+  {
+    key = 'v',
+    mods = 'CTRL',
+    action = act.PasteFrom 'Clipboard',
+  },
+}
+
+
 return config
