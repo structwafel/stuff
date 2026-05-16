92c6e75f23fc77a7f6c73abfb3fd37743eec9368
structwafel <ditisluca@yahoo.it>
2026-05-16 16:39:52 +0200

    colours in yay default

--- installs/install_yay.sh
+++ installs/install_yay.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+# Enable colors + niceties in pacman/yay output
+sudo sed -i 's/^#Color$/Color/; s/^#VerbosePkgLists$/VerbosePkgLists/; /^Color$/a ILoveCandy' /etc/pacman.conf
